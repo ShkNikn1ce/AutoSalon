3fa73b6f0442f0e5a4d5609fc09036c6d3eff704
Nikita Shkodin <shkodinnr@gmail.com>
2026-07-15 19:24:24 +0700

    Continue work of Swagger and environment

--- src/main/java/com/Nikita/AutoSalon/controller/CarController.java
+++ src/main/java/com/Nikita/AutoSalon/controller/CarController.java
@@ -21,16 +21,14 @@ import java.util.List;
 @RequestMapping("/cars")
 public class CarController {
     private final CarService carService;
-
     @Operation(
             summary = "Создать новый автомобиль",
             description = "Создает запись о новом автомобиле в системе"
     )
     @ApiResponses(value={
-            @ApiResponse(responseCode = "200", description = "Автомобиль успешно создан"),
+            @ApiResponse(responseCode = "201", description = "Автомобиль успешно создан"),
             @ApiResponse(responseCode = "400", description = "Неверные данные запроса"),
-            @ApiResponse(responseCode = "409", description = "Автомобиль с таким VIN уже существует")
-
+            @ApiResponse(responseCode = "500", description = "Такой автомобиль уже существует")
     }
 
     )
@@ -40,26 +38,72 @@ public class CarController {
         return carService.createCar(request);
     }
 
-    @GetMapping("/search")
-    public CarResponse getCarByVin(@RequestParam String vin){
-        return carService.findCarByVin(vin);
+    @Operation(
+            summary = "Вывести список всех автомобилей",
+            description = "Выводит список всех автомобилей"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешный вывод списка всех автомобилей"),
+            @ApiResponse(responseCode = "404", description = "Указанный путь не найден")
     }
 
-    @GetMapping("/{carId}")
-    public CarResponse getCarById(@PathVariable Long carId){
-        return carService.findCarById(carId);
-    }
-
+    )
     @GetMapping
     public List<CarResponse> getAllCars(){
         return carService.findAllCars();
     }
 
+    @Operation(
+            summary = "Поиск автомобиля по VIN",
+            description = "Возвращает информацию об автомобиле по его уникальному VIN-номеру"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Автомобиль найден"),
+            @ApiResponse(responseCode = "404", description = "Автомобиль с таким VIN не найден")
+    }
+    )
+    @GetMapping("/search")
+    public CarResponse getCarByVin(@RequestParam String vin){
+        return carService.findCarByVin(vin);
+    }
+
+    @Operation(
+            summary = "Получить автомобиль по ID",
+            description = "Возвращает информацию об автомобиле по его внутреннему ID"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Автомобиль найден"),
+            @ApiResponse(responseCode = "404", description = "Автомобиль с таким ID не найден")
+    }
+
+    )
+    @GetMapping("/{carId}")
+    public CarResponse getCarById(@PathVariable Long carId){
+        return carService.findCarById(carId);
+    }
+
+    @Operation(
+            summary = "Удалить автомобиль",
+            description = "Удаляет автомобиль из системы по VIN-номеру"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Автомобиль успешно удален"),
+            @ApiResponse(responseCode = "404", description = "Автомобиль с таким VIN не найден")
+    })
     @DeleteMapping("/{vin}")
     public void deleteCar(@PathVariable String vin){
         carService.deleteCar(vin);
     }
 
+    @Operation(
+            summary = "Обновить информацию об автомобиле",
+            description = "Обновляет данные существующего автомобиля по его ID"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Информация обновлена"),
+            @ApiResponse(responseCode = "500", description = "Неверные данные запроса"),
+            @ApiResponse(responseCode = "500", description = "Автомобиль с таким ID не найден")
+    })
     @PutMapping("/{carId}")
     public CarResponse updateCar(@PathVariable Long carId, @RequestBody UpdateCarRequest carRequest){
         return carService.updateInfoCar(carId,carRequest);
