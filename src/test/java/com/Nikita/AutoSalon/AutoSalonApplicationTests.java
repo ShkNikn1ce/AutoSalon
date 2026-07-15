89299ca24b3459a79aaddc691782a3483d16c2c6
Nikita Shkodin <shkodinnr@gmail.com>
2026-07-15 19:28:18 +0700

    Continue work of Swagger and environment

--- src/test/java/com/Nikita/AutoSalon/AutoSalonApplicationTests.java
+++ src/test/java/com/Nikita/AutoSalon/AutoSalonApplicationTests.java
@@ -1,13 +1,7 @@
 package com.Nikita.AutoSalon;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AutoSalonApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
